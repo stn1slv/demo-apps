1ea3b2e607d66b3411e357fefdf58496d53e94f1
Stanislav Deviatov <stanislav_deviatov@epam.com>
2021-06-23 09:25:22 +0300

    Parallel processing and aggregation responses

--- TripBooking/src/main/java/com/example/demo/MySimpleCamelRouter.java
+++ TripBooking/src/main/java/com/example/demo/MySimpleCamelRouter.java
@@ -4,6 +4,7 @@ import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.processor.aggregate.GroupedExchangeAggregationStrategy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,18 +19,18 @@ public class MySimpleCamelRouter extends RouteBuilder {
                 .to("direct:bookTrip");
 
         from("direct:bookTrip")
-                .log(LoggingLevel.INFO, "Book trip request")
-                .log("Sending ${body} with correlation key ${header.myId}")
-                .multicast().to("seda:car", "seda:hotel", "seda:flight")
-                .setBody(simple("Ok"));
-//                .log("${body}");
-        from("seda:car").to("http://carbooking:8080/camel/bookCar?bridgeEndpoint=true").to("seda:tripAggregator");
-        from("seda:flight").to("http://flightbooking:8080/camel/bookFlight?bridgeEndpoint=true").to("seda:tripAggregator");
-        from("seda:hotel").to("http://hotelbooking:8080/camel/bookHotel?bridgeEndpoint=true").to("seda:tripAggregator");
-
-        from("seda:tripAggregator")
-                .aggregate(constant(true), new MergeAggregationStrategy())
-                .completionSize(3)
-                .log("${body}");
+                .routeId("bookTrip-http")
+                .routeDescription("This is demo service for demonstration telemetry aspects")
+                .log(LoggingLevel.INFO, "New book trip request with traceId=${header.x-b3-traceid}")
+                .multicast(new MergeAggregationStrategy()).parallelProcessing()
+                        // .to("http://localhost:8081/camel/bookCar?bridgeEndpoint=true")
+                        // .to("http://localhost:8082/camel/bookFlight?bridgeEndpoint=true")
+                        // .to("http://localhost:8083/camel/bookHotel?bridgeEndpoint=true")
+                        .to("http://carbooking:8080/camel/bookCar?bridgeEndpoint=true")
+                        .to("http://flightbooking:8080/camel/bookFlight?bridgeEndpoint=true")
+                        .to("http://hotelbooking:8080/camel/bookHotel?bridgeEndpoint=true")
+                .end()
+                .log(LoggingLevel.INFO,"Response: \n${body}")
+                .unmarshal().json(JsonLibrary.Jackson);
     }
 }
